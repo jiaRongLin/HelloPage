f7e13862d09cb6eebead6e43f2822b1be584ef31
Jia <vernalin1422@gmail.com>
2022-12-29 15:38:01 +0800

    draw modify

--- HelloPage/Draw.cs
+++ HelloPage/Draw.cs
@@ -15,24 +15,24 @@ namespace HelloPage
         public Draw()
         {
             InitializeComponent();
-            Graphics g = this.pictureBox1.CreateGraphics();
-            g.Dispose();
+            
         }
-        Pen draw_pen = new Pen(Color.Black);
+        
         private void button1_Click(object sender, EventArgs e)
         {
             
             colorDialog1.ShowDialog();
             button2.BackColor = colorDialog1.Color;
-            draw_pen.Color = colorDialog1.Color;
-            draw_pen.Dispose();
+            
         }
-        int x;
-        int y;
+
         private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
         {
-            x=e.X;
-            y=e.Y;
+            Graphics g = this.pictureBox1.CreateGraphics();
+            g.Dispose();
+            Pen draw_pen = new Pen(Color.Black);
+            draw_pen.Color = colorDialog1.Color;
+            draw_pen.Dispose();
         }
     }
 }
